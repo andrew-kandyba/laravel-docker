f44f2cd15c29da486a898bdf9bf9c62f55be45f1
Andrew Kandyba <andrew.kandyba@gmail.com>
2023-01-19 22:49:06 +0200

    Added php container

---
+++ environment/containers/php/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/ash
+
+# composer install
+
+# # php artisan migrate
+# php artisan key:generate
+# php artisan optimize:clear
+# php artisan storage:link
+# php artisan route:cache
+# php artisan event:cache
+# php artisan view:cache
+
+# php artisan queue:restart
+
+# /usr/bin/supervisord -c /etc/supervisor/supervisor.conf
+
+exec "$@"
